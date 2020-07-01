4d6f4a4233dcb6f1b5d794a3dc4752111d53e7f1
Mario Neto <netto.mtx@gmail.com>
2020-07-01 19:20:56 -0300

    inserção da imagem do pokemon ao passar o mouse

--- poki-app/src/app/views/list/list.component.ts
+++ poki-app/src/app/views/list/list.component.ts
@@ -15,13 +15,23 @@ export class ListComponent implements OnInit {
   //   },
   // };
   // myList = [1, 2, 3, 4, 5];
+  selectedPkm = null;
   pokemonList = [
     { name: 'Bulbasaur', number: 1 },
     { name: 'Charmander', number: 4 },
     { name: 'Squirtle', number: 7 },
     { name: 'Pikachu', number: 25 },
   ];
+
+  get pkmSprite() {
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectPokemon(pkm) {
+    this.selectedPkm = pkm;
+  }
 }
