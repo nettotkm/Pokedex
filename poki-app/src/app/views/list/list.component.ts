65fe9d526c646580ecd8aaa2c763b94d2a67cffc
Mario Neto <netto.mtx@gmail.com>
2020-07-01 23:23:57 -0300

    consumindo Api e refatorando o código

--- poki-app/src/app/views/list/list.component.ts
+++ poki-app/src/app/views/list/list.component.ts
@@ -1,35 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from '../../services/pokeapi.service';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
+interface GitHubResponse {
+  inconplete_results: boolean;
+  items: any[];
+  total_count: number;
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-  // nome = 'Akira';
-  // isVisible = false;
-  // myvalue = '';
-  // aluno = {
-  //   dados: {
-  //     nome: 'Joao',
-  //   },
-  // };
-  // myList = [1, 2, 3, 4, 5];
+  searchText = '';
+  projects = [];
+  nameFilter = '';
   selectedPkm = null;
-  pokemonList = [
-    { name: 'Bulbasaur', number: 1 },
-    { name: 'Charmander', number: 4 },
-    { name: 'Squirtle', number: 7 },
-    { name: 'Pikachu', number: 25 },
-  ];
+  get pokemonList() {
+    return this.pokeapi.pokeList.filter((pokemon) => {
+      return (
+        pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1
+      );
+    });
+  }
 
   get pkmSprite() {
     const number = ('000' + this.selectedPkm.number).slice(-3);
     return `//serebii.net/sunmoon/pokemon/${number}.png`;
   }
-  constructor() {}
+  constructor(private pokeapi: PokeapiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    console.log('aqui');
+    // tslint:disable-next-line: no-unused-expression
+    this.pokeapi.listAll();
+  }
 
   selectPokemon(pkm) {
     this.selectedPkm = pkm;
