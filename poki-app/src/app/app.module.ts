0ef883b4cd34e76e6ee9c0b99a0fafbba5cf8040
Mario Neto <netto.mtx@gmail.com>
2020-07-06 20:15:45 -0300

    componentizando a template que mostra os pokemons

--- poki-app/src/app/app.module.ts
+++ poki-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent],
+  declarations: [AppComponent, ListComponent, ListItemComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
